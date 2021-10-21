ec9232680bca5b82a8b030ca67ea9c93ee88c040
Lyj <3505984383@qq.com>
2021-10-21 23:11:31 +0800

    second commit

--- src/homework/Hello.java
+++ src/homework/Hello.java
@@ -5,6 +5,6 @@ package homework;
  */
 public class Hello {
     public static void main(String[] args){
-        System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
